1b906e2dde85385f1dffcc99d6e8e96347bb7248
Nick Muerdter <nick.muerdter@nrel.gov>
2014-03-03 17:02:11 -0700

    Adding cookbook to install the yum-plugin-security package.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'yum-security'
+maintainer       'Nick Muerdter'
+maintainer_email 'nick.muerdter@nrel.gov'
+license          'MIT'
+description      'Installs/Configures yum-security'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: yum-security
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "yum-plugin-security"
